08f46e965737cce360bf34810919adcbc59ac9d6
jianglei <2062578173@qq.com>
2018-09-17 09:47:27 +0800

    日志修改

--- src/main/java/com/jy/register/EurekaServer.java
+++ src/main/java/com/jy/register/EurekaServer.java
@@ -1,11 +1,11 @@
-package com.synapse.register;
+package com.jy.register;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * Created by wangyifan on 2017/3/1.
+ * Created by jainglei on 2017/6/1.
  */
 @SpringBootApplication
 @EnableEurekaServer
